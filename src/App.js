ae0a5954f4e20cbb8b1dab2372f798072b712bfe
leewebstechnologies <aliuajibade317@gmail.com>
2022-12-14 12:00:45 -0800

    topbar folder created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+// import Home from "./pages/home/Home";
+import PersonIcon from '@mui/icons-material/Person';
+
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div>
+      {/* <Home /> */}
+      <PersonIcon />
     </div>
   );
 }
